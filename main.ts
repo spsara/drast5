740ec60ce88b0cc5cba8ebd1637435e9c6c79261
spsara <90417807+spsara@users.noreply.github.com>
2021-09-09 23:51:53 +0300

    Αρχικά αρχεία για έργο MakeCode

---
+++ main.ts
@@ -0,0 +1,35 @@
+basic.forever(function () {
+    if (input.compassHeading() < 45) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (input.compassHeading() < 135) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (input.compassHeading() < 225) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
